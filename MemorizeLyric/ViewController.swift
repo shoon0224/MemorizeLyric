4f0710082db88bbe936b0ca3547f5765502b2206
shoon0224 <dltkdgns204@naver.com>
2020-07-20 13:41:03 +0900

    Initial Commit

---
+++ MemorizeLyric/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemorizeLyric
+//
+//  Created by 이상훈 on 2020/07/20.
+//  Copyright © 2020 이상훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
